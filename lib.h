f8e5207b0f2bc4462aebe12a33f190ac24720cd9
harumlokawati <harumlokawati@yahoo.com>
2017-10-17 20:42:33 +0700

    Add files via upload

---
+++ lib.h
@@ -0,0 +1,16 @@
+#ifndef _LIB_H_
+#define _LIB_H_
+
+#define SOH 1       /* Start of Header Character */
+#define STX 2       /* Start of Text Character */
+#define ETX 3       /* End of Text Character */
+#define ENQ 5       /* Enquiry Character */
+#define ACK 6       /* Acknowledgement */
+#define BEL 7       /* Message Error Warning */
+#define CR 13       /* Carriage Return */
+#define LF 10       /* Line Feed */
+#define NAK 21      /* Negative Acknowledgement */
+#define Endfile 26  /* End of file character */
+#define ESC 27      /* ESC key */
+
+#define ACK_TIMEOUT 3
