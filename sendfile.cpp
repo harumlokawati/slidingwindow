c081a236e59db028440bce88d24d0e8a6fe463a6
harumlokawati <harumlokawati@yahoo.com>
2017-10-18 03:09:34 +0700

    Add files via upload
    edit some

--- sendfile.cpp
+++ sendfile.cpp
@@ -16,7 +16,7 @@
 #include <ctime>
 #include <mutex>
 #include <thread>
-
+#include <vector>
 using namespace std;
 #define NIL -999
 #define DEFAULT_BUFF 10;
@@ -25,17 +25,18 @@ using namespace std;
 	struct sockaddr_storage serverStorage;
 	socklen_t addr_size;
 	char file[1000];
-	framesender Buffer[100];
-	framesender* Window;
+	vector<framesender> Buffer;
+	framesender Window[100];
 	//timebuff* timeBuffer;
 	int lengthFile = 0;
 	int indexFile= 0;
 	int Socket;
 	int sizeBuffer = DEFAULT_BUFF;
-	int sizeWindoew = DEFAULT_WIN;
-	int SWS, LAR, LFS = 0;
-	int countSentBuffer = 0;
-	int countPendingACK = 0;
+	int sizeWindow = DEFAULT_WIN;
+	int LAR, LFS = 0;
+	//int SentBuffer = 0;
+	int PendingACK = 0;
+	mutex mut;
 
 void configureSetting(char IP[], int portNum) {
 	  serverAddr.sin_family = AF_INET;
@@ -63,17 +64,51 @@ void sendtoBuffer(){
 	for(int i=0; (i<sizeBuffer)&&(indexFile<lengthFile); i++){
 		framesender fs(i+1);
 		fs.setData(file[i]);
-		Buffer[0] = fs;
+		Buffer.push_back(fs);
 		indexFile++;
 		cout<<file[i]<<endl;
 	}
 }
 
-void SEND(){
-	sendtoBuffer();
+
+void delfirst(framesender A[],int length){
+	for(int i = 1; i<length; i++){
+		A[i-1]=A[i];
+	}
 }
-
-
+void delbyseqnum(framesender A[], int length, int Seq_Num){
+	bool deleted=false;
+	int i = 0;
+	while((!deleted)&&(i<length)){
+		if (A[i].getSeq_Num() == Seq_Num){
+			deleted=true;
+			int j=i;
+			while(j<(length-1)){
+				A[j]=A[j+1];
+				j++;
+			}
+			A[j]=0;
+		}
+		i++;
+	}
+}
+framesender getframebyseqnum(framesender A[], int length, int Seq_Num){
+	int i = 0;
+	int index = 0;
+	bool found = false;
+	while(i < length){
+		if(A[i].getSeq_Num() == Seq_Num ){
+			found = true;
+			index = i;
+		}
+		i++;
+	}
+	if(found){
+		return A[index];
+	}else{
+		return 0;
+	}
+}
 void sendFrame(framesender fs){
 	char* msg = fs.toBytes();
 	int msgLength = fs.getBytesLength(); 
@@ -84,24 +119,83 @@ void sendFrame(framesender fs){
 	printf("Message ke-%d: %c\n", fs.getSeq_Num(), fs.getData());
 	sendto(Socket,msg,300,0,(struct sockaddr *)&serverAddr,addr_size);	
 }
+void init(){
+	sendtoBuffer();
+	LFS = 0;
+	LAR = 0;
+	int idx_win = 0;
+	mut.lock();
+	for (int i = 0; i < sizeWindow; i++) {
+		sendFrame(Buffer[0]);
+		Window[idx_win] = Buffer[0];
+		idx_win++;
+		Buffer.erase(Buffer.begin());
+	}
+	mut.unlock();
+}
+
+
+void receiveACK(){
+	char * frame;
+	while(1){
+		recvfrom(Socket,frame,10,0,NULL,NULL);
+		framereceiver recvmsg(frame);
+		if(recvmsg.getACK()==ACK){
+			printf("ACK[%d]\n",recvmsg.getSeq_Num());
+			//jika ACK yang diterima untuk frame awal window
+			if(Window[0].getSeq_Num() == recvmsg.getSeq_Num()){
+				//frame dihapus dari window
+				delfirst(Window,sizeWindow);
+				for(int i=0;i<=PendingACK;i++){
+					if(!Buffer.empty()){
+						sendFrame(Buffer[0]);
+						mut.lock();
+						Window[sizeWindow-1]=(Buffer[0]);
+						Buffer.erase(Buffer.begin());
+						mut.unlock();
+					}
+				}
+				PendingACK=0;
+			}else{
+				PendingACK++;
+				mut.lock();
+				delbyseqnum(Window, sizeWindow, recvmsg.getSeq_Num());
+				mut.unlock();
+			}
+
+		}else if(recvmsg.getACK()==NAK){
+			printf("NAK[%d]\n",recvmsg.getSeq_Num());
+			sendFrame(getframebyseqnum(Window,sizeWindow,recvmsg.getSeq_Num()));
+		}
+	}
+}
+void SEND(){
+	thread thread1(receiveACK,Socket);
+	thread thread2(init);
+	//thread thread3(timeout);
+	thread1.join();
+	thread2.join();
+	//thread3.join();
+}
 int main(int argc, char* argv[]) {
 	printf("%d\n",argc);
 	printf("%s%s%s%s%s%s\n",argv[0],argv[1],argv[2],argv[3],argv[4],argv[5]);
 	if (argc != 6) {
-    	cout << "Usage : ./sendfile <filename> <sindowsize> <buffersize> <destination_ip> <destination_port>" << endl;
+    	cout << "Usage : ./sendfile <filename> <windowsize> <buffersize> <destination_ip> <destination_port>" << endl;
   	} else {
 	    int port;
 	    char * filename;
 
 	    port = atoi(argv[5]);
 	    filename = argv[1];
-
-	    // Membuat UDP socket
+	    sizeBuffer = atoi(argv[3]);
+	    sizeWindow = atoi(argv[2]);
+	    // UDP socket
 	    Socket = socket(PF_INET, SOCK_DGRAM, 0);
-	    cout << "Membuat socket untuk koneksi ke " << argv[1] << ":" << port << endl;
+	    cout << "socket untuk koneksi ke " << argv[4] << ":" << port << endl;
 	    configureSetting(argv[4], port);
 	    cout << "1 udah sampe" << endl;
-	    // Inisialisasi ukuran variabel yang akan digunakan
+
 	    addr_size = sizeof serverAddr;  
 	    readFile(filename);
 	    cout << "sampe 2" <<endl;
@@ -111,3 +205,4 @@ int main(int argc, char* argv[]) {
 
   return 0;
 }
+ v
